913fb619dd76979318309706732c06e39b4922b3
beanstown106 <nbenis106@gmail.com>
2015-05-03 03:50:51 -0400

    Build Google audio files from android12 [1/2]
    Change-Id: Ib20341d418c9f827ab86e60d52724d96b8d7e31a
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -17,13 +17,13 @@
 # Includes all AOSP product packages
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
+$(call inherit-product-if-exists, frameworks/base/data/sounds/GoogleAudio.mk)
 
-# Default AOSP sounds
-ifeq ($(CHERISH_BUILD),)
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
-else
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
-endif
+# Additional settings used in all AOSP builds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.ringtone?=Your_new_adventure.ogg \
+    ro.config.notification_sound?=Eureka.ogg \
+    ro.com.android.dataroaming?=true \
 
 # More AOSP packages
 PRODUCT_PACKAGES += \
